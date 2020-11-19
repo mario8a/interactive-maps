ada04f49515e16826e66aa1f7504282a370950f1
mario8a <mario8ato@hotmail.com>
2020-11-19 00:45:00 -0600

    Mostrar una sola card listo

--- js/map.js
+++ js/map.js
@@ -1,9 +1,12 @@
 var wordStates = document.querySelectorAll(".list-of-states li");
+var svgStates = document.querySelectorAll ( ".states" );
 
 wordStates.forEach(function(el) {
 
   el.addEventListener("mouseenter", function() {
+    //saca el codigo o atributo o id del la lista
     var stateCode = el.getAttribute("data-state");
+    //Relaciona el id con el id que tenga el elemento del mapa
     var svgState = document.querySelector("#" + stateCode);
     el.classList.add("on");
     svgState.classList.add("on");
@@ -14,6 +17,24 @@ wordStates.forEach(function(el) {
     var svgState = document.querySelector("#" + stateCode);
     el.classList.remove("on");
     svgState.classList.remove("on");
+  });
+
+});
+
+svgStates.forEach(function(el) {
+
+  el.addEventListener("mouseenter", function() {
+    var stateId = el.getAttribute("id");
+    var wordState = document.querySelector("[data-state='" + stateId + "']");
+    el.classList.add("on");
+    wordState.classList.add("on");
+  });
+
+  el.addEventListener("mouseleave", function() {
+    var stateId = el.getAttribute("id");
+    var wordState = document.querySelector("[data-state='" + stateId + "']");
+    el.classList.remove("on");
+    wordState.classList.remove("on");
   });
 
 });
